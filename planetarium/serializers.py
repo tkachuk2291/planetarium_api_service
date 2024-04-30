25252b0ad59165cf6acf7de614e338bfc9ec4d5f
Anton Tkachuk <tkacuk2291@gmail.com>
2024-04-30 11:42:20 +0300

    create files urls.py & serializers.py

---
+++ planetarium/serializers.py
@@ -0,0 +1,21 @@
+# from django.db import transaction
+# from rest_framework import serializers
+# from rest_framework.exceptions import ValidationError
+#
+# from planetarium.models import (
+#     Ticket,
+# )
+#
+#
+# class GenreSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Genre
+#         fields = ("id", "name")
+#
+#
+# class ActorSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Actor
+#         fields = ("id", "first_name", "last_name", "full_name")
+
+
