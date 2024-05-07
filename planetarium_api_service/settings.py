c75199f1140d0f48501edff4a7305edea0c29745
Anton Tkachuk <tkacuk2291@gmail.com>
2024-05-07 16:03:03 +0300

    implementing docker logic

--- planetarium_api_service/settings.py
+++ planetarium_api_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("USERS_SECRET_KEY",)
+SECRET_KEY = os.environ.get("USERS_SECRET_KEY",)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
