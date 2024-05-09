2a9daa8b7bdc2cc25d43cdc0b41e9446d6de7cc3
Anton Tkachuk <tkacuk2291@gmail.com>
2024-05-10 01:23:39 +0300

    changed db to PostgresSql

--- planetarium_api_service/settings.py
+++ planetarium_api_service/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1" , "c4a4-176-37-22-78.ngrok-free.app" ]
-
+ALLOWED_HOSTS = ["127.0.0.1", "d0e4-176-37-22-78.ngrok-free.app"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -79,24 +78,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "planetarium_api_service.wsgi.application"
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.environ.get("POSTGRES_DB"),
-#         "USER": os.environ.get("POSTGRES_USER"),
-#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-#         "HOST": os.environ.get("POSTGRES_HOST"),
-#         "PORT": os.environ.get("POSTGRES_PORT"),
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -175,4 +166,3 @@ SPECTACULAR_SETTINGS = {
         "defaultModelsExpandDepth": 2,
     },
 }
-
